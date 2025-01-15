6639335d08a1fafce8696951e966ffa43d8411fb
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-01-15 21:43:28 +0530

    .

--- Assets/script/ScoreManager.cs
+++ Assets/script/ScoreManager.cs
@@ -7,18 +7,7 @@ public class ScoreManager : MonoBehaviour
 {
     float score;
     public TextMeshProUGUI Score_text;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+ 
     public void InceaseScore(float Increment)
     {
         score += Increment;
